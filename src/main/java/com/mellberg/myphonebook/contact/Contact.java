03cd09225805091d729b4fe1b704855f07c99ed6
Filip Mellberg <filipmellberg@outlook.com>
2022-03-25 14:15:01 +0100

    .

---
+++ src/main/java/com/mellberg/myphonebook/contact/Contact.java
@@ -0,0 +1,54 @@
+package com.mellberg.myphonebook.contact;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Contact {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "contact_id", nullable = false)
+    private int contactId;
+    @Basic
+    @Column(name = "first_name", nullable = false, length = 20)
+    private String firstName;
+    @Basic
+    @Column(name = "last_name", nullable = true, length = 30)
+    private String lastName;
+    @Basic
+    @Column(name = "phone_nr", nullable = true)
+    private Integer phoneNr;
+
+    public int getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(int contactId) {
+        this.contactId = contactId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getPhoneNr() {
+        return phoneNr;
+    }
+
+    public void setPhoneNr(Integer phoneNr) {
+        this.phoneNr = phoneNr;
+    }
+    
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mellberg.myphonebook {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.persistence;
 
 
     opens com.mellberg.myphonebook to javafx.fxml;
